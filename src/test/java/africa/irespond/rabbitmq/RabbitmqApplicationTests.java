bd1780d3a3d449c50dd2f31e2d9fc57d7f33889d
oladejomubarak <oladejomubarakade@gmail.com>
2023-06-13 10:07:04 +0100

    creating producer and consumer configuration

---
+++ src/test/java/africa/irespond/rabbitmq/RabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package africa.irespond.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
